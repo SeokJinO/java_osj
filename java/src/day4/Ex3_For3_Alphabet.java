812f1b393bcd15cef94feab64ab9b332632f5f82
SeokJinO <104331411+SeokJinO@users.noreply.github.com>
2022-04-28 16:58:34 +0900

    자바 4일차 : for문

---
+++ java/src/day4/Ex3_For3_Alphabet.java
@@ -0,0 +1,21 @@
+package day4;
+
+public class Ex3_For3_Alphabet {
+
+	public static void main(String[] args) {
+		/* 반복문을 이용하여 a부터 z까지 출력하는 코드를 작성하세요.
+		 * 
+		 */
+		char ch = 65;
+		for(int i=65; i<91; i++) {
+			System.out.print((char)i);
+		}
+			System.out.println(" ");
+			char j;
+		for(j = 'a'; j<= 'z'; j++) {
+			System.out.print(j);
+		}
+		}
+	}
+
+
