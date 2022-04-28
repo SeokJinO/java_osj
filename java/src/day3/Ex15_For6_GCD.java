c4b7e3d76ce6687433779fe59c39f823013b0e5e
SeokJinO <104331411+SeokJinO@users.noreply.github.com>
2022-04-28 10:31:17 +0900

    자바 3일차 : GCD

--- java/src/day3/Ex15_For6_GCD.java
+++ java/src/day3/Ex15_For6_GCD.java
@@ -21,8 +21,7 @@ public class Ex15_For6_GCD {
 		int num1 = 12 , num2 = 18;
 		
 		for(i = 1; i<=num1; i++) {
-			if(num1%i==0)
-			if(num2%i==0)
+			if(num1%i==0&&num2%i==0)
 					gcd=i;
 		}System.out.println(gcd);
 	}
