b2fe8bc9f9844b377cd20dc9097bc18c1251f7b0
SeokJinO <104331411+SeokJinO@users.noreply.github.com>
2022-05-06 17:19:34 +0900

    자바 10일차 : 과제 리모트컨트롤러

--- java/src/day10/Ex1_Class_RemoteController.java
+++ java/src/day10/Ex1_Class_RemoteController.java
@@ -8,3 +8,12 @@ public class Ex1_Class_RemoteController {
 	}
 
 }
+
+/* Tv 리모컨 클래스를 생성하고, 객체를 생성 후 기능들을 테스트 해 보세요. */
+	// 전원 버튼
+	// 채널 이동 위아래
+	// 채널 넘버 이동
+	// 스피커 음량 조절
+class RemoteController{
+	
+}
