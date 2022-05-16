bc920a2285d87d27a6503ee236dc9b0002899f13
SeokJinO <104331411+SeokJinO@users.noreply.github.com>
2022-05-16 17:30:48 +0900

    자바 15일차 - 예외 예제 - try catch 예제 - NullPointerException 예제

---
+++ java/src/day15/Ex9_Exception_NullPointer.java
@@ -0,0 +1,26 @@
+package day15;
+
+public class Ex9_Exception_NullPointer {
+
+	public static void main(String[] args) {
+		/* 문자열이 주어지면 주어진 문자열에 a가 몇개 있는지 
+		 * 확인하는 코드를 작성하세요.
+		 * abca => 2
+		 * abc => 1
+		 * bc => 0*/
+		
+		String str = null;
+		int count = 0;
+		try {
+			for(int i =0; i<str.length(); i++) {
+				if(str.charAt(i) == 'a') {
+					count++;
+				}
+			}
+		}catch(NullPointerException a) {
+			System.out.println("예외 발생");
+		}
+		System.out.println(count);
+	}
+
+}
