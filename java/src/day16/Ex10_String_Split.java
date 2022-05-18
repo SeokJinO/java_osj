b7387a16ac8ef26bf739595cc7a00d193930267d
SeokJinO <104331411+SeokJinO@users.noreply.github.com>
2022-05-18 17:04:11 +0900

    자바 16일차 : String 예제 - split 예제

---
+++ java/src/day16/Ex10_String_Split.java
@@ -0,0 +1,22 @@
+package day16;
+
+public class Ex10_String_Split {
+
+	public static void main(String[] args) {
+		/*  -문자열.split(정규표현식) : 문자열에 정규표현식과 일치하는 것들이 있으면 잘라서 배열로 만듦
+		 *  - String arr[] = "abc,def,fgh".split(",")를 하면
+		 *    String arr[] = {"abc", "def", "fgh"}; 가 저장된것과 같음*/
+		String hobby = "독서,음악감상,영화감상-운동";
+		String hobbies[] = hobby.split("[,-]");
+		System.out.println("취미 : ");
+		if(hobbies != null) {
+			for(String tmp : hobbies) {
+				System.out.print(tmp + " ");
+			}
+			System.out.println();
+		}else {
+			System.out.println("없음");
+		}
+	}
+
+}
