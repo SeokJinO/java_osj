6c3379fb92c8418e5808bb94aef114f2700faa7a
SeokJinO <104331411+SeokJinO@users.noreply.github.com>
2022-05-19 17:39:53 +0900

    자바 18일차 : List를 이용한 학생 관리 프로그램 예제

---
+++ java/src/day18/Ex03_StudentMain.java
@@ -0,0 +1,14 @@
+package day18;
+
+import java.util.Scanner;
+
+public class Ex03_StudentMain {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		StudentManager sm = new StudentManager(sc);
+		sm.run();
+	}
+
+}
