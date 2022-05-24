9b7e48547a7312c37570193b167505f7b6f31fbd
SeokJinO <104331411+SeokJinO@users.noreply.github.com>
2022-05-24 17:16:54 +0900

    자바 21일차 : 람다식 예제 - Supplier 예제

---
+++ java/src/day21/Ex05_Lambda_Supplier.java
@@ -0,0 +1,18 @@
+package day21;
+
+import java.util.function.Supplier;
+
+public class Ex05_Lambda_Supplier {
+
+	public static void main(String[] args) {
+		int min = 1, max = 10;
+		Supplier<Integer> su = () -> (int) (Math.random() * 10 + 1);
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(su.get());
+		}
+	}
+	public static int random(int min, int max) {
+		return (int)(Math.random()*(max - min +1)+min);
+	}
+}
