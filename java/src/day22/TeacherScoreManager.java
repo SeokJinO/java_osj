c6d63e168c57f779ba210acfab3bab2024052ed4
SeokJinO <104331411+SeokJinO@users.noreply.github.com>
2022-05-31 14:28:36 +0900

    자바 22일차 : (강사)성적관리 프로그램 예제(1명 학생)

--- java/src/day22/TeacherScoreManager.java
+++ java/src/day22/TeacherScoreManager.java
@@ -13,9 +13,9 @@ public class TeacherScoreManager implements ConsoleProgram {
 	private Scanner scan;
 	private int exitMenu = 4;
 	private List<TeacherScore> list = new ArrayList<TeacherScore>();
-	
+
 	public TeacherScoreManager(Scanner scan) {
-		if(scan == null)
+		if (scan == null)
 			scan = new Scanner(System.in);
 		this.scan = scan;
 	}
@@ -34,9 +34,9 @@ public class TeacherScoreManager implements ConsoleProgram {
 
 	@Override
 	public void excute(int menu) {
-		switch(menu) {
+		switch (menu) {
 		case 1:
-			if(addScore()) 
+			if (addScore())
 				printMessage("성적을 추가했습니다.");
 			else
 				printMessage("이미 등록된 과목 성적입니다.");
@@ -45,6 +45,7 @@ public class TeacherScoreManager implements ConsoleProgram {
 			printScore();
 			break;
 		case 3:
+			modifyScore();
 			break;
 		case 4:
 			break;
@@ -52,7 +53,10 @@ public class TeacherScoreManager implements ConsoleProgram {
 		}
 	}
 
+
+
 	private void printScore() {
+		sort();
 		System.out.println("-----------------------");
 		System.out.println("성적 확인 메뉴");
 		System.out.println("1. 모든 성적 확인");
@@ -61,40 +65,51 @@ public class TeacherScoreManager implements ConsoleProgram {
 		System.out.println("-----------------------");
 		System.out.print("메뉴 선택 : ");
 		int menu = scan.nextInt();
-		switch(menu) {
+		switch (menu) {
 		case 1:
-			printScoreList(s->true);
+			printScoreList(s -> true);
 			break;
 		case 2:
 			System.out.print("학년 : ");
 			int grade = scan.nextInt();
 			System.out.print("학기 : ");
 			int term = scan.nextInt();
-			printScoreList(s->s.getGrade() == grade && s.getTerm() == term);
+			printScoreList(s -> s.getGrade() == grade && s.getTerm() == term);
 			break;
 		case 3:
 			System.out.print("과목 : ");
 			String name = scan.next();
-			printScoreList(s->s.getName().equals(name));
+			printScoreList(s -> s.getName().equals(name));
 			break;
 		default:
 			printMessage("잘못된 메뉴를 선택했습니다.");
 		}
 	}
+
+	private void sort() {
+		list.sort((TeacherScore s1, TeacherScore s2)-> {
+			if(s1.getGrade() != s2.getGrade())
+				return s1.getGrade() - s2.getGrade();
+			if(s1.getTerm() != s2.getTerm())
+				return s1.getTerm() - s2.getTerm();
+			return s1.getName().compareTo(s2.getName());
+		});
+	}
+
 	private void printScoreList(Predicate<TeacherScore> p) {
 		List<TeacherScore> tmpList = new ArrayList<TeacherScore>();
-		
-		for(TeacherScore tmp : list) {
-			if(p.test(tmp)) {
+
+		for (TeacherScore tmp : list) {
+			if (p.test(tmp)) {
 				tmpList.add(tmp);
 			}
 		}
-		
-		if(tmpList.size() == 0) {
+
+		if (tmpList.size() == 0) {
 			printMessage("출력할 성적이 없습니다.");
-		}else {
+		} else {
 			System.out.println("-----------------------");
-			for(TeacherScore tmp : tmpList) {
+			for (TeacherScore tmp : tmpList) {
 				System.out.println(tmp);
 			}
 			System.out.println("-----------------------");
@@ -111,10 +126,10 @@ public class TeacherScoreManager implements ConsoleProgram {
 		int term = scan.nextInt();
 		System.out.print("과목 : ");
 		String name = scan.next();
-		
+
 		//학년, 학기, 과목이 같은 성적이 성적 리스트에 있으면 있다고 알림
 		TeacherScore tmp = new TeacherScore(grade, term, name);
-		if(list.contains(tmp)) {
+		if (list.contains(tmp)) {
 			System.out.println("-----------------------");
 			return false;
 		}
@@ -125,7 +140,7 @@ public class TeacherScoreManager implements ConsoleProgram {
 		int finals = scan.nextInt();
 		System.out.print("수행 : ");
 		int pa = scan.nextInt();
-		
+
 		//성적 정보를 성적 리스트에 추가
 		tmp = new TeacherScore(grade, term, name, midterm, finals, pa);
 		list.add(tmp);
@@ -139,25 +154,60 @@ public class TeacherScoreManager implements ConsoleProgram {
 		list.add(new TeacherScore(1, 2, "국어", 90, 100, 90));
 		list.add(new TeacherScore(1, 1, "영어", 100, 80, 90));
 		list.add(new TeacherScore(1, 1, "수학", 70, 100, 90));
-		
+
 		int menu = -1;
 		do {
 			try {
 				menu = selectMenu(scan);
 				excute(menu);
-			}catch(InputMismatchException e) {
+			} catch (InputMismatchException e) {
 				printMessage("올바른 값을 입력하세요.");
 				scan.nextLine();
-			}catch(RuntimeException e) {
+			} catch (RuntimeException e) {
 				printMessage(e.getMessage());
-			}catch(Exception e) {
+			} catch (Exception e) {
 				printMessage("예외 발생 : " + e.getMessage());
 			}
-		}while(menu != exitMenu);
+		} while (menu != exitMenu);
 	}
+
 	private void printMessage(String str) {
 		System.out.println("-----------------------");
 		System.out.println(str);
 		System.out.println("-----------------------");
+	}
+	private boolean modifyScore() {
+		System.out.println("-----------------------");
+		System.out.println("수정할 학생 정보를 입력하세요.");
+		//학년, 학기, 과목을 입력
+		System.out.print("학년 : ");
+		int grade = scan.nextInt();
+		System.out.print("학기 : ");
+		int term = scan.nextInt();
+		System.out.print("과목 : ");
+		String name = scan.next();
+
+		//학년, 학기, 과목이 같은 성적이 성적 리스트에 없으면 없다고 알림
+		TeacherScore tmp = new TeacherScore(grade, term, name);
+		int index = list.indexOf(tmp);
+		if (index < 0) {
+			System.out.println("-----------------------");
+			return false;
+		}
+		//중간,기말,수행평가을 입력
+		System.out.print("중간 : ");
+		int midterm = scan.nextInt();
+		System.out.print("기말 : ");
+		int finals = scan.nextInt();
+		System.out.print("수행 : ");
+		int pa = scan.nextInt();
+
+		//리스트에서 학년, 학기, 과목이 같은 성적 정보를 가져옴.
+		tmp = list.get(index);
+		//가져온 성적 정보를 수정
+		tmp.setMidterm(midterm);
+		tmp.setFinals(finals);
+		tmp.setPerformanceAssessment(pa);
+		return true;
 	}
 }
