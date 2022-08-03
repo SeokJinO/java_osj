8b4d95b95d63ee3f50c4a85a972534d3e22480e8
SeokJinO <104331411+SeokJinO@users.noreply.github.com>
2022-08-03 16:54:26 +0900

    spring : DB 연동 테스트

--- spring_osj/src/main/java/kr/green/spring/service/MemberService.java
+++ spring_osj/src/main/java/kr/green/spring/service/MemberService.java
@@ -1,8 +1,14 @@
 package kr.green.spring.service;
- 
+
+import kr.green.spring.vo.MemberVO;
+
 public interface MemberService {
 
 	String getEmail(String id);
+
+	MemberVO getMember(String me_id);
+
+	MemberVO getMember(MemberVO member);
 	
 }
  
