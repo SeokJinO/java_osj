c06fd411636273be8c25d77072ef6b099f2bc40e
SeokJinO <104331411+SeokJinO@users.noreply.github.com>
2022-08-05 15:06:54 +0900

    spring : 게시글 상세 기능 구현

--- spring_osj/src/main/java/kr/green/spring/service/BoardService.java
+++ spring_osj/src/main/java/kr/green/spring/service/BoardService.java
@@ -10,5 +10,9 @@ public interface BoardService {
 	void insertBoard(BoardVO board, MemberVO user);
 
 	ArrayList<BoardVO> getBoardList();
+	
 
+	void updateViews(Integer bd_num);
+
+	BoardVO getBoard(Integer bd_num);
 }
--- spring_osj/src/main/java/kr/green/spring/service/BoardServiceImp.java
+++ spring_osj/src/main/java/kr/green/spring/service/BoardServiceImp.java
@@ -17,18 +17,18 @@ public class BoardServiceImp implements BoardService {
 
 	@Override
 	public void insertBoard(BoardVO board, MemberVO user) {
-		if(board == null || board.getBd_title() == null || board.getBd_content() == null)
+		if (board == null || board.getBd_title() == null || board.getBd_content() == null)
 			return;
 		//제목에 공백만 있는 경우
-		if(board.getBd_title().trim().length() == 0)
+		if (board.getBd_title().trim().length() == 0)
 			return;
 		//내용에 공백만 있는 경우
-		if(board.getBd_content().trim().length() == 0)
+		if (board.getBd_content().trim().length() == 0)
 			return;
 		//로그인 안한 경우
-		if(user == null || user.getMe_id() == null)
+		if (user == null || user.getMe_id() == null)
 			return;
-		
+
 		//게시글 작성자로 회원 아이디를 저장
 		board.setBd_me_id(user.getMe_id());
 		boardDao.insertBoard(board);
@@ -37,5 +37,20 @@ public class BoardServiceImp implements BoardService {
 	@Override
 	public ArrayList<BoardVO> getBoardList() {
 		return boardDao.selectBoardList();
+	}
+
+	@Override
+	public void updateViews(Integer bd_num) {
+		if (bd_num == null)
+			return;
+		boardDao.updateViews(bd_num);
+
+	}
+
+	@Override
+	public BoardVO getBoard(Integer bd_num) {
+		if (bd_num == null)
+			return null;
+		return boardDao.selectBoard(bd_num);
 	}
 }
