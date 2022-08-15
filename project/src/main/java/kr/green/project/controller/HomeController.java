252f0475c56c1b89f049eb2bb554beec97ca09b1
SeokJinO <104331411+SeokJinO@users.noreply.github.com>
2022-08-16 00:27:20 +0900

    project 기본 설정 완료

--- project/src/main/java/kr/green/project/controller/HomeController.java
+++ project/src/main/java/kr/green/project/controller/HomeController.java
@@ -1,14 +1,21 @@
 package kr.green.project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import kr.green.project.service.MemberService;
 
 @Controller
 public class HomeController {
+
+	@Autowired
+    MemberService memberService;
 	
-	@RequestMapping(value= "/")
-	public ModelAndView openTilesView(ModelAndView mv) throws Exception{
+	@RequestMapping(value= "/", method=RequestMethod.GET)
+	public ModelAndView home(ModelAndView mv){
 	    mv.setViewName("/main/home");
 	    mv.addObject("setHeader", "타일즈");
 	    return mv;
